d91a992302e5c7df3ba5b42a513abaa98a1a6193
BeaudrotLab <beaudrot@rice.edu>
2019-04-18 15:32:30 -0500

    Changed yaxis label

---
+++ Figures.R
@@ -0,0 +1,11 @@
+# Purpose: Demo for GitHub lab session
+# Author: Lydia Beaudrot
+# Date: April 18, 2019
+
+
+# Load example fox data file
+data <- read.csv(file="foxes.csv")
+
+# Plot food availability as a function of home range size
+plot(data$AVFOOD~data$AREA)
+plot(data$AVFOOD~data$AREA, ylab="Food availbility")
